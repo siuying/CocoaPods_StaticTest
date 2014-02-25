eaede5606da9f1c8adb681fba2843dd9ce7773f8
Francis Chong <francis@ignition.hk>
2014-02-25 22:05:33 +0800

    Initial Commit

---
+++ TestStatic/IGViewController.h
@@ -0,0 +1,13 @@
+//
+//  IGViewController.h
+//  TestStatic
+//
+//  Created by Francis Chong on 2/25/14.
+//  Copyright (c) 2014 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGViewController : UIViewController
+
+@end
